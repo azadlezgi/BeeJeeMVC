3391d7d1cd6dbc421efbb6a006f746a47aaec48a
Azad <azadlezgi@yandex.ru>
2022-04-15 23:57:03 +0400

    Routers Yes Page & 404 page

---
+++ application/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'index' => [
+        'controller'    => 'app',
+        'action'        => 'index',
+    ],
+
+    'account/login' => [
+        'controller'    => 'acount',
+        'action'        => 'login',
+    ],
+];
--- application/core/Router.php
+++ application/core/Router.php
@@ -10,20 +10,36 @@ class Router
 
     public function __construct()
     {
-        //
+        $routes_arr = require 'application/config/routes.php';
+        foreach ($routes_arr as $key => $val) {
+            $this->add($key, $val);
+        }
     }
 
-    public function add() {
-
+    public function add($route, $params) {
+        $route = '#^'. $route .'$#';
+        $this->routes[$route] = $params;
     }
 
     public function match() {
-
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
 
     public function run() {
-        echo "Starttt";
+
+        if ($this->match()) {
+            echo "Yes Page";
+        } else {
+            echo "404 Page";
+        }
     }
 
 }
